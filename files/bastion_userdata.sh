8bf16202a529f1a363bff274e41cad021d44faa8
CosmicQ <jason@cosmicegg.net>
2019-02-28 14:13:18 -0500

    Adding echo statements for debugging

--- files/bastion_userdata.sh
+++ files/bastion_userdata.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# The echo statements are here to help debug what is going on.  To see results
+# look at /var/log/cloud-init-output.log when the instance is up.
 
 #update
+echo "Getting update and installing aws-cli"
 yum update -y && yum install -y aws-cli
+echo "Installing Cloudwatch agent"
+rpm -Uvh https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 
 # Set vars
+echo "Declaring variables"
 declare -rx REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[a-z]$//')
 declare -rx NUM=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 |awk -F. '{print $3"-"$4}')
 declare -rx INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
@@ -14,17 +20,21 @@ declare -rx ASSOCIATION_ID=$(aws --region $REGION ec2 describe-addresses --query
 declare -rx ALLOCATION_ID=$(aws --region $REGION ec2 describe-addresses --query 'Addresses[].AllocationId[]' --filters "Name=tag:Name,Values=BastionEIP" --output text)
 
 #set hostname
+echo "Setting hostname"
 sed -i "s/HOSTNAME=localhost.localdomain/HOSTNAME=${NAME}-${NUM}/" /etc/sysconfig/network
 hostname $NAME-$NUM
 
 # Detach EIP from any other instance and attach to this instance
+echo "Doing EIP Magic"
 aws --region $REGION ec2 disassociate-address --association-id $ASSOCIATION_ID
 aws --region $REGION ec2 associate-address --instance-id $INSTANCE_ID --allocation-id $ALLOCATION_ID 
 
 # Keep the SSH_CLIENT environment variable
+echo "Adding lines to /etc/sudoers"
 echo "Defaults env_keep += \"SSH_CLIENT\"" >> /etc/sudoers
 
 # Send commands to /var/log/commands
+echo "Adding lines to /etc/bashrc"
 cat <<'EOC'>> /etc/bashrc
 export SSH_CLIENT=${SSH_CLIENT}
 declare -rx IP=$(echo $SSH_CLIENT | awk '{print $1}')
@@ -32,13 +42,14 @@ declare -rx BASTION_LOG=/var/log/commands
 declare -rx PROMPT_COMMAND='history -a >(logger -t "ON: $(date)   [FROM]:${IP}   [USER]:${USER}   [PWD]:${PWD}" -s 2>>${BASTION_LOG})'
 EOC
 
+# Creating /var/log/commands
+echo "Creating /var/log/commands and setting permissions"
 touch /var/log/commands
 chmod 662 /var/log/commands
 
-rpm -Uvh https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-
 # Send log files to cloudwatch logs
-if [ -e /opt/aws/amazon-cloudwatch-agent/etc ]; then
+if [ -e /opt/aws/amazon-cloudwatch-agent/etc ]&&[ -ne /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json ]; then
+  echo "Cloudwatch agent is installed, configure and start it"
   cat <<EOF >> /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 {
     "logs": {
@@ -83,6 +94,7 @@ EOF
 fi
 
 # Add cron.  Do security updates, but no kernel updates since those require a reboot
+echo "Updating root cron"
 cat <<'EOC'>> /var/spool/cron/root
 */10 * * * * echo -e '127.0.0.1\tlocalhost localhost.localdomain' > /etc/hosts && /usr/bin/aws ec2 describe-instances --region ", "Ref": "AWS::Region" },
  --filters Name=instance-state-name,Values=running --query 'Reservations[].Instances[].[PrivateIpAddress,Tags[?Key==`Name`].Value[]]' --output text | sed '$!N;s/\\n/\\t/' |sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 >> /etc/hosts
