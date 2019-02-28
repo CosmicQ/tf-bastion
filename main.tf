f80a81f90784f77321b196db05bf75766b707256
CosmicQ <jason@cosmicegg.net>
2019-02-28 11:59:18 -0500

    changing amazon AMI to v2

--- main.tf
+++ main.tf
@@ -33,17 +33,16 @@ variable "bastion_user_data" {
 
 ##########################################################
 # Get AMI
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners           = ["amazon"]
+data "aws_ami" "amazon-linux-2" {
+ most_recent = true
 
-  filter {
-    name      = "name"
-    values    = ["amzn-ami-hvm-*-x86_64-gp2"]
-  }
+ filter {
+   name   = "owner-alias"
+   values = ["amazon"]
+ }
 
-  filter {
-    name      = "owner-alias"
-    values    = ["amazon"]
-  }
+ filter {
+   name   = "name"
+   values = ["amzn2-ami-hvm*"]
+ }
 }
