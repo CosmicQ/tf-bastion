6abea34697f3c12ffbafee1ae01be566fc8cd255
CosmicQ <jason@cosmicegg.net>
2019-03-01 18:27:49 -0500

    added ending curly braces for var in the policy

--- main.tf
+++ main.tf
@@ -181,7 +181,10 @@ resource "aws_iam_policy" "policy" {
             "Action": [
                 "s3:*"
             ],
-            "Resource": "arn:aws:s3:::${var.bastion-s3-bucket/*"
+            "Resource": [
+                         "arn:aws:s3:::${var.bastion-s3-bucket}",
+                         "arn:aws:s3:::${var.bastion-s3-bucket}/*"
+                        ]
         },
         {
             "Sid": "bastion2",
