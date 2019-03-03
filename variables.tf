f6e36e4fa21c6601367e13c0652e82f83ed8824c
CosmicQ <jason@cosmicegg.net>
2019-03-03 12:39:46 -0500

    Fixed the perioud that this IDE added in for me.  Thanks for nothing

--- variables.tf
+++ variables.tf
@@ -3,42 +3,42 @@
 variable "vpc_id" { }
 
 variable "public_subnets" {
-  type    = "list"
+  type        = "list"
   #["${element(module.vpc.public_subnets, 0)}", "${element(module.vpc.public_subnets, 1)}"]
 }
 variable "bastion_name" {
-  type    = "string"
-  default = "bastion"
+  type        = "string"
+  default     = "bastion"
 }
 variable "bastion_key_name" {
-  type    = "string"
-  default = "default"
+  type        = "string"
+  default     = "default"
 }
 variable "bastion_ingress" {
-  default = ["0.0.0.0/0"]
+  default     = ["0.0.0.0/0"]
 }
 variable "bastion_instance_type" {
-  default = "t2.nano"
+  default     = "t2.nano"
 }
 variable "bastion_min_size" {
-  default = "1"
+  default     = "1"
 }
 variable "bastion_max_size" {
-  default = "1"
+  default     = "1"
 }
 variable "bastion_desired_capacity" {
-  default = "1"
+  default     = "1"
 }
 variable "bastion_user_data" {
-  default = "files/bastion_userdata.sh"
+  default     = "files/bastion_userdata.sh"
 }
 
 variable "bastion_s3_bucket" {}
 
 variable "bastion_dns" {
   description = "Domain name to use for bastion server (foo.com not bastion.foo.com)"
-  type.       = "string"
-  default = ""
+  type        = "string"
+  default     = ""
 }
 
 variable "create_bastion_dns" {
@@ -50,8 +50,8 @@ variable "create_bastion_dns" {
 ##########################################################
 # Get AMI
 data "aws_ami" "amazon_linux2" {
- most_recent = true
- owners      = ["amazon"]
+ most_recent  = true
+ owners       = ["amazon"]
 
  filter {
    name   = "owner-alias"
