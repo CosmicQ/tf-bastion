d37935769007ea3a1cd47ccc7e4a7d861bb60e3e
Jason Qualkenbush <69851532+jqualkenbush-fmi@users.noreply.github.com>
2021-06-21 09:54:39 -0400

    Fixing main.tf for new version of terraform

--- variables.tf
+++ variables.tf
@@ -78,8 +78,10 @@ data "aws_ami" "amazon_linux2" {
 ##########################################################
 # Get userdata
 data "template_file" "userdata" {
-  template = file(var.bastion_user_data)
-  vars = {
-    bastion_name = var.bastion_name
-  }
+  template = templatefile(
+    "${var.bastion_user_data}",
+    {
+      bastion_name = var.bastion_name
+    }
+  )
 }
