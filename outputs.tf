d520a716f85a283f75b0e7d67a777ff2cc2e652e
Jason Qualkenbush <69851532+jqualkenbush-fmi@users.noreply.github.com>
2021-06-21 09:26:27 -0400

    Fixing main.tf for new version of terraform

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 # Security Group ID
 output "bastion_sg_id" {
   description = "The security group of the bastion host."
-  value       = "${module.bastion_sg.this_security_group_id}"
+  value       = "${module.bastion_sg.security_group_id}"
 }
 
 # Elastic IP
