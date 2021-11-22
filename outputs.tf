7b379cff7eddfa1c23a2a64b8912e685c224335f
CosmicQ <jason@cosmicegg.net>
2021-11-22 16:59:06 -0500

    Adding IP and DNS to the outputs

--- outputs.tf
+++ outputs.tf
@@ -4,8 +4,20 @@ output "bastion_sg_id" {
   value       = "${module.bastion_sg.security_group_id}"
 }
 
-# Elastic IP
+# Elastic ID
 output "bastion_eip_id" {
-  description = "The elastic IP of the bastion host."
+  description = "The elastic IP ID"
   value       = "${aws_eip.bastion-eip.id}"
+}
+
+# Elastic IP
+output "bastion_eip_ip" {
+  description = "The IP address of the Bastion EIP"
+  value       = "${aws_eip.bastion-eip.public_ip}"
+}
+
+# Elastic DNS
+output "bastion_eip_dns" {
+  description = "The DNS of the Bastion EIP"
+  value       = "${aws_eip.bastion-eip.public_dns}
 }
